83ca4d8a9d0a3792c10f8369498fe322fe6230ff
YingqiTao <yingqitao2000@gmail.com>
2018-08-09 14:27:22 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,24 +1,24 @@
 #METHOD 1
-def least_coins(cents)
-  number_of_coins = []
-  coins_value = [25, 10, 5, 1]
-  least_coins = {}
-  remainder = cents
-  coins_value_index = 0
+# def least_coins(cents)
+#   number_of_coins = []
+#   coins_value = [25, 10, 5, 1]
+#   least_coins = {}
+#   remainder = cents
+#   coins_value_index = 0
   
-  while remainder != 0
-    number_of_coins.push(remainder/coins_value[coins_value_index])
-    remainder = remainder % coins_value[coins_value_index]
-    coins_value_index += 1
-  end
+#   while remainder != 0
+#     number_of_coins.push(remainder/coins_value[coins_value_index])
+#     remainder = remainder % coins_value[coins_value_index]
+#     coins_value_index += 1
+#   end
   
-  least_coins[:quarters] = number_of_coins[0]
-  least_coins[:dimes] = number_of_coins[1]
-  least_coins[:nickels] = number_of_coins[2]
-  least_coins[:pennies] = number_of_coins[3]
+#   least_coins[:quarters] = number_of_coins[0]
+#   least_coins[:dimes] = number_of_coins[1]
+#   least_coins[:nickels] = number_of_coins[2]
+#   least_coins[:pennies] = number_of_coins[3]
   
-  return least_coins
-end
+#   return least_coins
+# end
 
 #METHOD 2 
 def least_coins(cents)
@@ -29,7 +29,7 @@ def least_coins(cents)
   dimes = cents/10
   cents %= 10
   
-  nickles = cents/5 
+  nickels = cents/5 
   cents %= 5 
   
   pennies = cents/1 
@@ -37,5 +37,10 @@ def least_coins(cents)
   
   least_coins[:quarters] = quarters
   least_coins[:dimes] = dimes
-  least_coins[:nickels] = nickles
+  least_coins[:nickels] = nickels
   least_coins[:pennies] = pennies
+  
+  return cents, least_coins
+end
+
+puts least_coins(93)
